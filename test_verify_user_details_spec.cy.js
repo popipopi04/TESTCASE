d9dbf15922efdcf826e37a2cd0b31610a2176912
Pradeeepa <165149795+popipopi04@users.noreply.github.com>
2024-08-02 14:14:08 +0530

    Add files via upload

---
+++ test_verify_user_details_spec.cy.js
@@ -0,0 +1,39 @@
+const endPoint = require("../../fixtures/reqres_api_end_points.json")
+const user_module = require("../../support/user")
+
+const write_result_to = "cypress/output/user_details.json";
+const url = Cypress.config('baseUrl')
+
+describe('Test case is to verify User details through http requests', ()=> {
+
+    beforeEach(()=> { 
+        // This hook will verify whether the app is up and running before executing each test case
+        cy.log("Verify API url");
+        cy.getAPIResponse(Cypress.config('baseUrl'));
+    })
+
+    // This test case will write received user details to a JSON file
+    it('Write user details to json', ()=> {
+        cy.log("Inside test case!!");
+        cy.log(write_result_to);
+        user_module.writeUserDetailsToJson(write_result_to, url+endPoint.user_details)
+    })
+
+    // This test case will compare received user details with expected values
+    it('Verify user details', ()=> {
+        cy.fixture('testdata_expected_users').then((exp_users) => {
+            cy.log('Inside fixture');
+            cy.log(exp_users.total);
+            user_module.verifyUserDetails(exp_users,url+endPoint.user_details)
+            
+        })
+        // cy.log(url+endPoint.user_details);
+        // cy.visit(url);
+        // cy.get('.url').scrollIntoView().click({force: true})
+        // cy.get("body pre").then('user_details_received', ()=> {
+        //     cy.log(user_details_received)
+        // })
+        // user_module.verifyUserDetails(url+endPoint.user_details)
+
+    })
+});
